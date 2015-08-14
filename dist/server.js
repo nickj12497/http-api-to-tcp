a0f2a7a67ddc09bd7bfde710f85c78a5a749e8bd
nickj12497 <njohnson@adelyte.com>
2015-08-14 12:24:59 -0400

    Updated server.coffee

--- dist/server.js
+++ dist/server.js
@@ -1,18 +1,22 @@
 (function() {
-  var net, server;
+  var net, server, status;
 
   net = require('net');
+
+  status = '';
 
   server = net.createServer(function(socket) {
     console.log('Client Connected...');
     return socket.on('data', function(data) {
       console.log(data.toString());
       if (data.toString().trim().toLowerCase() === 'play') {
-        return socket.write('Now Playing...\r\n');
+        status = 'Playing';
+        console.log('PLAY\x1F\r\n');
+        return socket.write('STATUS: \x1FPlaying \x1F00:00:00\x1F-00:21:22\r\nTITLE: \x1FBojack Horseman\x1F\r\n');
       } else if (data.toString().trim().toLowerCase() === 'pause') {
-        return socket.write('Paused...\r\n');
+        return socket.write('STATUS: \x1FPaused \x1F at 00:14:52\x1F-00:21:22\r\nTITLE: \x1FBojack Horseman\x1F\r\n');
       } else if (data.toString().trim().toLowerCase() === 'stop') {
-        return socket.write('Stopped Playing...\r\n');
+        return socket.write('STATUS: \x1FStopped\r\nReturning to Main Menu\x1F\r\n');
       } else if (data.toString().trim().toLowerCase() === 'exit' || 'goodbye') {
         socket.write('Diconnecting now...\r\n');
         return socket.destroy();
--- src/server.coffee
+++ src/server.coffee
@@ -1,16 +1,18 @@
 net = require('net')
-
+status = ''
 server = net.createServer (socket) ->
 	console.log('Client Connected...')
 
 	socket.on('data', (data) ->
 		console.log(data.toString())
 		if data.toString().trim().toLowerCase() is 'play'
-			socket.write('Now Playing...\r\n')
+			status = 'Playing'
+			console.log('PLAY\x1F\r\n')
+			socket.write('STATUS: \x1FPlaying \x1F00:00:00\x1F-00:21:22\r\nTITLE: \x1FBojack Horseman\x1F\r\n')
 		else if data.toString().trim().toLowerCase() is 'pause'
-			socket.write('Paused...\r\n')
+			socket.write('STATUS: \x1FPaused \x1F at 00:14:52\x1F-00:21:22\r\nTITLE: \x1FBojack Horseman\x1F\r\n')
 		else if data.toString().trim().toLowerCase() is 'stop'
-			socket.write('Stopped Playing...\r\n')
+			socket.write('STATUS: \x1FStopped\r\nReturning to Main Menu\x1F\r\n')
 		else if data.toString().trim().toLowerCase() is 'exit' or 'goodbye'
 			socket.write('Diconnecting now...\r\n')
 			socket.destroy()
